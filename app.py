15abc719c112dc98cad0807371474877b282bf61
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2025-06-10 12:02:13 -0300

    feat: Atualizando dataset

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request
-import flask
 import numpy as np
 import google.generativeai as generativeai
 from google import genai
@@ -11,70 +10,71 @@ import os
 
 load_dotenv()
 app = Flask(__name__)
-CORS(app) # Initialize CORS for the entire application
+CORS(app)  # Initialize CORS for the entire application
 
 model = 'models/gemini-embedding-exp-03-07'
-modeloEmbeddings = pickle.load(open('datasetEmbedding2025.pkl','rb'))
+modeloEmbeddings = pickle.load(open('datasetEmbeddings2025.pkl','rb'))
 chave_secreta = os.getenv('GOOGLE_API_KEY')
 generativeai.configure(api_key=chave_secreta)
-def gerarBuscarConsulta(consulta, dataset):
-	embedding_consulta = generativeai.embed_content(
-		model=model,
-		content=consulta,
-		task_type="retrieval_query",
-	)
+print(chave_secreta)
 
-	produtos_escalares = np.dot(np.stack(dataset["Embeddings"]),
-								embedding_consulta['embedding'])  # Calculo de distancia entre consulta e a base
-	print(embedding_consulta)
-	print(produtos_escalares)
-	indice = np.argmax(produtos_escalares)
-	print(produtos_escalares[indice])
-	return dataset.iloc[indice]['Conteúdo']
+def gerarBuscarConsulta(consulta,dataset):
+    embedding_consulta = generativeai.embed_content(model=model,
+                                content=consulta,
+                                task_type="retrieval_query",
+                                )
+    produtos_escalares = np.dot(np.stack(dataset["Embeddings"]), embedding_consulta['embedding']) # Calculo de distancia entre consulta e a base
+    print(embedding_consulta)
+    print(produtos_escalares)
+    indice = np.argmax(produtos_escalares)
+    print(produtos_escalares[indice])
+    return dataset.iloc[indice]['Conteúdo']
 
 def melhorarResposta(inputText):
-	client = genai.Client(api_key=chave_secreta)
-	model = "gemini-1.5-flash"
-	contents = [
-		types.Content(
-			role="user",
-			parts=[
-				types.Part.from_text(text=inputText),
-			],
-		),
-	]
-	generate_content_config = types.GenerateContentConfig(
-		temperature=1,
-		top_k=32,
-		response_mime_type="text/plain",
-		system_instruction=[
-			types.Part.from_text(text="""Considere a consulta e resposta, reescreva as sentenças de resposta de uma forma alternativa, não apresente opções de reescrita"""),
-		],
-	)
-	response = client.models.generate_content(
-		model=model,
-		contents=contents,
-		config=generate_content_config,
-	)
-	return response.text
+    client = genai.Client(api_key=chave_secreta)
+    model = "gemini-1.5-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text=inputText),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        temperature=1,
+        top_k=32,
+        response_mime_type="text/plain",
+        system_instruction=[
+            types.Part.from_text(text="""Considere a consulta e resposta, reescreva as sentenças de resposta de uma forma alternativa, não apresente opções de reescrita"""),
+        ],
+    )
+    response = client.models.generate_content(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    )
+    return response.text;
 
 @app.route("/")
 def home():
-	consulta = "Quem é você ?"
-	resposta = gerarBuscarConsulta(consulta, modeloEmbeddings)
-	prompt = f"Consulta: {consulta} Resposta: {resposta}"
-	response = melhorarResposta(prompt)
-	return response
+    consulta = "Onde fica?"
+    resposta = gerarBuscarConsulta(consulta, modeloEmbeddings)
+    prompt = f"Consulta: {consulta} Resposta: {resposta}"
+    response = melhorarResposta(prompt)
+    return response
+
 
 @app.route("/api", methods=["POST"])
 def results():
-	# Verifique a chave de autorização
-	auth_key = request.headers.get("Authorization")
-	if auth_key != chave_secreta:
-		return jsonify({"error": "Unauthorized"}), 401
-	data = request.get_json(force=True)
-	consulta = data["consulta"]
-	resultado = gerarBuscarConsulta(consulta, modeloEmbeddings)
-	prompt = f"Consulta: {consulta} Resposta: {resultado}"
-	response = melhorarResposta(prompt)
-	return jsonify({"mensagem": response})
+    # Verifique a chave de autorização
+    auth_key = request.headers.get("Authorization")
+    if auth_key != chave_secreta:
+        return jsonify({"error": "Unauthorized"}), 401
+    data = request.get_json(force=True)
+    consulta = data["consulta"]
+    resultado = gerarBuscarConsulta(consulta, modeloEmbeddings)
+    prompt = f"Consulta: {consulta} Resposta: {resultado}"
+    response = melhorarResposta(prompt)
+    return jsonify({"mensagem":  response})
+
--- gerarEmbeddings.py
+++ gerarEmbeddings.py
@@ -5,28 +5,32 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-chave_secreta = os.getenv('API_KEY')
+chave_secreta = os.getenv('GOOGLE_API_KEY')
 print(chave_secreta)
 generativeai.configure(api_key=chave_secreta)
-csv_url = 'https://docs.google.com/spreadsheets/d/1pr9QbEENce-9NFrTtLdrfUd_swzQoOs0FDbShaqMECQ/export?format=csv&id=1pr9QbEENce-9NFrTtLdrfUd_swzQoOs0FDbShaqMECQ'
 
+csv_url = 'https://docs.google.com/spreadsheets/d/1pr9QbEENce-9NFrTtLdrfUd_swzQoOs0FDbShaqMECQ/export?format=csv'
+df = pd.read_csv(csv_url)
 print(df.head())
 
 model = 'models/gemini-embedding-exp-03-07'
-def gerarEmbeddings(title, text):
+def gerarEmbeddings(title, text): 
   result = generativeai.embed_content(model=model,
-                                  content=text,
+                                content=text,
                                 task_type="retrieval_document",
                                 title=title)
   return result['embedding']
 
 
-
-df["Embeddings"] = df.apply(lambda row: gerarEmbeddings(row["Titulo"],row["Conteúdo"]), axis=1)
+import time 
+def gerarEmbeddingsComDelay(title, text, delay=10):
+    time.sleep(delay)  # Atraso de 1 segundo
+    return gerarEmbeddings(title, text)
+df["Embeddings"] = df.apply(lambda row: gerarEmbeddingsComDelay(row["Titulo"],row["Conteúdo"]), axis=1)
 print(df)
 
 import pickle
-pickle.dump(df, open('datasetEmbedding2025.pkl','wb'))
+pickle.dump(df, open('datasetEmbeddings2025.pkl','wb'))
 
-modeloEmbeddings = pickle.load(open('datasetEmbedding2025.pkl','rb'))
+modeloEmbeddings = pickle.load(open('datasetEmbeddings2025.pkl','rb'))
 print(modeloEmbeddings)
